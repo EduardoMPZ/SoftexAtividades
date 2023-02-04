7a451e96e10ac5bf3b0947f0d85f1fb2b0233757
Eduardo Mendes Paz <edu7020@gmail.com>
2023-02-04 16:05:06 -0300

    Atividade 01 web service

--- React/reactproject6/src/App.js
+++ React/reactproject6/src/App.js
@@ -1,12 +1,16 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
 import Add from './pagina01';
 import A from './pagina02';
 
 function App() {
     return (
         <div>
-            <h1>Layout Commom</h1>
+            <h1>Layout Common</h1>
+            <Link to="/">Home  </Link>
+            <Link to="/teste01">Página 01  </Link>
+            <Link to="/teste02">Página 02</Link>
             <Routes>
+                <Route exact path="/" />
                 <Route exact path="/teste01" element={<Add />} />
                 <Route exact path="/teste02" element={<A />} />
             </Routes>
