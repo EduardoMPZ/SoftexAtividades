a8b5adc04fd7c5e9008e3f336555ea2e32c52f0a
Eduardo Mendes Paz <edu7020@gmail.com>
2023-02-05 17:29:26 -0300

    Teste de serviço web em c# com aplicação no console

---
+++ Web Services/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ServiceReference1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        WebService1SoapClient obj;
+        obj = new WebService1SoapClient(default);
+
+        Console.WriteLine("Hello, World!\n");
+
+        string ans = obj.Add();
+
+        Console.WriteLine("\nExecutando a Função Add:" + ans);
+
+        ans = obj.Subtract();
+
+        Console.WriteLine("\nExecutando a Função Subtract:" + ans);
+
+        ans = obj.Multiply();
+
+        Console.WriteLine("\nExecutando a Função Multiply:" + ans);
+
+        ans = obj.Divide();
+
+        Console.WriteLine("\nExecutando a Função Divide:" + ans);
+    }
+}
---
+++ Web Services/WebApplication1/WebService1.asmx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebApplication1
+{
+    /// <summary>
+    /// Descrição resumida de WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que esse serviço da web seja chamado a partir do script, usando ASP.NET AJAX, remova os comentários da linha a seguir. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Olá, Mundo";
+        }
+
+        [WebMethod]
+        public string Add()
+        {
+            return "Add";
+        }
+
+        [WebMethod]
+        public string Subtract()
+        {
+            return "Subtract";
+        }
+
+        [WebMethod]
+        public string Multiply()
+        {
+            return "Multiply";
+        }
+
+        [WebMethod]
+        public string Divide()
+        {
+            return "Divide";
+        }
+    }
+}
